a673df1831ac7af235eb7be4417c3e06f8804f77
painter890602 <124643595+princepainter@users.noreply.github.com>
2025-11-14 14:37:30 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,8 @@
 from .painteri2v_for_kj import PainterI2VforKJ
 
-NODE_CLASS_MAPPINGS = {
-    "PainterI2VforKJ": PainterI2VforKJ,
-}
+NODE_CLASS_MAPPINGS = { "PainterI2VforKJ": PainterI2VforKJ, }
+NODE_DISPLAY_NAME_MAPPINGS = { "PainterI2VforKJ": "PainterI2V for KJ", }
 
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "PainterI2VforKJ": "PainterI2V for KJ",
-}
+__version__ = "1.06"
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
-
